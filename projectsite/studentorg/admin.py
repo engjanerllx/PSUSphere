ef8c3593d9c2e65dbea229b4300860589e78c60c
engjanerllx <lagancarla@gmail.com>
2024-11-20 02:47:44 +0800

    refactor admin,py

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -5,5 +5,28 @@ from .models import College, Program, Organization, Student, OrgMember
 admin.site.register(College)
 admin.site.register(Program)
 admin.site.register(Organization)
-admin.site.register(Student)
-admin.site.register(OrgMember)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("student_id", "lastname", "firstname", "middlename", "get_program")
+    search_fields = ("lastname", "firstname")
+
+    def get_program(self, obj):
+        # Assuming `program` is a related field or attribute in the Student model
+        return obj.program
+    get_program.short_description = 'Program'
+
+
+  
+@admin.register(OrgMember)
+class OrgMemberAdmin(admin.ModelAdmin):
+    list_display = ("student", "get_member_program", "organization", "date_joined")
+    search_fields = ("student__lastname", "student__firstname")
+
+    def get_member_program(self, obj):
+        try:
+            member = Student.objects.get(id=obj.student_id)
+            return member.program
+        except Student.DoesNotExist:
+            return None
+
