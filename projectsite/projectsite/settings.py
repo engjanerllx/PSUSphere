e2a95f67d95c0b2e9f9ac01d92d8bfac0fcbc6ec
engjanerllx <147321293+engjanerllx@users.noreply.github.com>
2024-11-20 23:38:01 +0800

    Update settings.py

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6ebk!0m=h)_katx*m#&@fs^8m-wi!mq$+591n60-bd-tco$+%a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['psusphere.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['janiyah.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
