010678c60e98aafedfa30d1f87dcc4830f26012d
engjanerllx <lagancarla@gmail.com>
2024-11-20 23:08:09 +0800

    modify allowed host

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6ebk!0m=h)_katx*m#&@fs^8m-wi!mq$+591n60-bd-tco$+%a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['psusphere.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
